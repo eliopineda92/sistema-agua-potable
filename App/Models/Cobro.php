f5f4aa0ee4f12d54ec1f5adf531a965af5f8c9ba
eliopineda92 <eliopinedacog@gmail.com>
2026-03-01 19:20:30 -0600

    cobro.php update

--- App/Models/Cobro.php
+++ App/Models/Cobro.php
@@ -2,19 +2,15 @@
 
 namespace App\Models;
 
+use App\Observers\CobroObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cobro extends Model
 {
-	 protected static function booted(): void
-    {
-        static::observe(CobroObserver::class);
-		static::observe(\App\Observers\CobroObserver::class);
-    }
     protected $fillable = [
         'cliente_id',
-		'periodo',
+        'periodo',
         'fecha_cobro',
         'monto',
         'mora',
@@ -24,11 +20,11 @@ class Cobro extends Model
         'observaciones',
     ];
 
-protected $casts = [
-    'fecha_cobro' => 'date',
-    'fecha_vencimiento' => 'date',
-    'fecha_pago' => 'datetime',
-];
+    protected $casts = [
+        'fecha_cobro' => 'date',
+        'fecha_vencimiento' => 'date',
+        'fecha_pago' => 'datetime',
+    ];
 
     protected $dates = [
         'fecha_cobro',
@@ -40,5 +36,9 @@ protected $casts = [
     {
         return $this->belongsTo(Cliente::class);
     }
-}
 
+    protected static function booted(): void
+    {
+        static::observe(CobroObserver::class);
+    }
+}
